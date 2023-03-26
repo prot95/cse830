22d947b00085d61616038d0c08eebd4021cab9e7
prot95 <protichi13075@iiitd.ac.in>
2023-03-25 23:08:18 -0400

    Checking in code

--- part2.py
+++ part2.py
@@ -1,4 +1,6 @@
 from timsort import tim_sort
+from part1 import merge_sort
+from part1 import insertion_sort
 import matplotlib.pyplot as plt
 import random
 import time
@@ -12,7 +14,7 @@ for i in range(1, 100001, 1000):
 # Test different values of k
 k_values = [8, 16, 32, 64, 128, 256]
 averageTimes = []
-for k in k_values:
+for k in range(10, 300, 10):
     print(f"Testing k = {k}")
     total_time = 0
     for arr in data:
@@ -25,10 +27,50 @@ for k in k_values:
     print(f"Average time taken: {avg_time:.6f} seconds")
 
 # Plot the data
-plt.plot(k_values, averageTimes)
+plt.plot(range(10, 300, 10), averageTimes)
 plt.xlabel('n')
 plt.ylabel('Time (s)')
-plt.title('Insertion Sort vs Merge Sort')
+plt.title('Tim sort vs K')
 plt.legend()
 # plt.show()
 plt.savefig('my_plot3.png')
+plt.close()
+def largeArguments():
+    avg_merge = []
+    avg_insertion = []
+    avg_timsort = []
+    for n in range(10, 1010, 10):
+        merge_times = []
+        insertion_times = []
+        tim_sort_times = []
+        for i in range(10):
+            arr = [random.randint(0, 1000) for _ in range(n)]
+            start = time.time()
+            merge_sort(arr)
+            end = time.time()
+            merge_times.append(end - start)
+            start = time.time()
+            insertion_sort(arr)
+            end = time.time()
+            insertion_times.append(end - start)
+            start = time.time()
+            tim_sort(arr, 30)
+            end = time.time()
+            tim_sort_times.append(end - start)
+
+        avg_merge.append(sum(merge_times) / len(merge_times))
+        avg_insertion.append(sum(insertion_times) / len(insertion_times))
+        avg_timsort.append(sum(tim_sort_times) / len(tim_sort_times))
+
+    # Plot the data
+    plt.plot(range(10, 1010, 10), avg_insertion, label='Insertion Sort')
+    plt.plot(range(10, 1010, 10), avg_merge, label='Merge Sort')
+    plt.plot(range(10, 1010, 10), avg_timsort, label='Tim Sort')
+    plt.xlabel('n')
+    plt.ylabel('Time (s)')
+    plt.title('Insertion Sort vs Merge Sort')
+    plt.legend()
+    #plt.show()
+    plt.savefig('timVSother.png')
+
+largeArguments()
